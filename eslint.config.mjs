eab0888dd29c1ae4d1abd7b1d948203783df3363
Chshine1 <chshine@qq.com>
2026-02-21 12:35:42 +0800

    init commit

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+// noinspection SpellCheckingInspection
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+// noinspection JSCheckFunctionSignatures
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs', 'dist', 'node_modules'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/no-extraneous-dependencies': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+);
