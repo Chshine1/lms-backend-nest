ee08dc60c8770f64c107728178e679128cd49c9e
Chshine1 <chshine@qq.com>
2026-03-27 17:07:09 +0800

    chore: Create migration scripts

---
+++ migrations/scripts/data-source.ts
@@ -0,0 +1,51 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { Admin } from '../../apps/user-service/src/entities/admin.entity';
+import { Campus } from '../../apps/user-service/src/entities/campus.entity';
+import { Parent } from '../../apps/user-service/src/entities/parent.entity';
+import { Student } from '../../apps/user-service/src/entities/student.entity';
+import { Teacher } from '../../apps/user-service/src/entities/teacher.entity';
+import { Tenant } from '../../apps/user-service/src/entities/tenant.entity';
+import { User } from '../../apps/user-service/src/entities/user.entity';
+import { UserPermission } from '../../apps/user-service/src/entities/user-permission.entity';
+import { Classroom } from '../../apps/course-service/src/entities/classroom.entity';
+import { Course } from '../../apps/course-service/src/entities/course.entity';
+import { CourseMaterial } from '../../apps/course-service/src/entities/course-material.entity';
+import { CourseVideo } from '../../apps/course-service/src/entities/course-video.entity';
+import { CourseSchedule } from '../../apps/course-scheduling-service/src/entities/course-schedule.entity';
+import { Enrollment } from '../../apps/course-enrollment-service/src/entities/enrollment.entity';
+import { File } from '../../apps/file-service/src/entities/file.entity';
+import { Review } from '../../apps/assignment-service/src/entities/review.entity';
+import { Submission } from '../../apps/assignment-service/src/entities/submission.entity';
+
+const options: DataSourceOptions = {
+  type: 'postgres',
+  host: 'postgres-migration',
+  port: 5432,
+  username: 'lms',
+  password: 'lms',
+  database: 'lms',
+  entities: [
+    Admin,
+    Campus,
+    Parent,
+    Student,
+    Teacher,
+    Tenant,
+    User,
+    UserPermission,
+    Classroom,
+    Course,
+    CourseMaterial,
+    CourseVideo,
+    CourseSchedule,
+    Enrollment,
+    File,
+    Review,
+    Submission,
+  ],
+  migrations: ['migrations/*.ts'],
+  synchronize: false,
+};
+
+// noinspection JSUnusedGlobalSymbols
+export default new DataSource(options);
---
+++ migrations/scripts/migrate.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+set -e
+NAME=$1
+
+if [ -z "${NAME}" ]; then
+  echo "Usage: $0 <migration-name>"
+  exit 1
+fi
+
+echo "Starting PostgreSQL"
+docker compose -f ./migrations/scripts/docker-compose.migrations.yml up -d postgres-migration
+
+echo "Waiting for PostgreSQL healthcheck..."
+CONTAINER_ID=$(docker compose -f ./migrations/scripts/docker-compose.migrations.yml ps -q postgres-migration)
+while [ "$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_ID")" != "healthy" ]; do
+  sleep 1s
+done
+
+echo "PostgreSQL ready, run migrations"
+docker compose -f ./migrations/scripts/docker-compose.migrations.yml run --env "MIGRATION_NAME=${NAME}" --rm migration
+
+echo "Release all resources"
+docker compose -f ./migrations/scripts/docker-compose.migrations.yml down -v
+
+echo "Migration complete"
