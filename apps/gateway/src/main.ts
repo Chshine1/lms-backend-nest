ec7e419ab27f429e6d97d814655a60103686ab4b
Chshine1 <chshine@qq.com>
2026-03-31 14:33:44 +0800

    chore: Add api documents for gateway application

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,10 +3,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigurationService } from '@app/infrastructure';
 import { GatewayConfig } from '@app/infrastructure';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = (): OpenAPIObject =>
+    SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const configService = app.get(ConfigurationService);
   const gatewayConfig = configService.getByKey('gateway', GatewayConfig);
