7da2e1fe6f9c5ebf93b785e2368e2b67af3ec6e2
shreyanshbardia <shreyanshbardia6@gmail.com>
2026-05-17 14:37:05 -0400

    Made DatabaseHealthIndicator optional depending on whether it is being injected to prevent runtime errors

--- libs/core/src/health/health.controller.ts
+++ libs/core/src/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Optional } from '@nestjs/common';
 import { DatabaseHealthIndicator } from './indicators/database.health';
 import { RabbitMQHealthIndicator } from './indicators/rabbitmq.health';
 import {
@@ -12,18 +12,28 @@ import {
 export class HealthController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
-    private readonly databaseHealthIndicator: DatabaseHealthIndicator,
+    @Optional() private readonly databaseHealthIndicator: DatabaseHealthIndicator,
     private readonly rabbitMQHealthIndicator: RabbitMQHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   async check(): Promise<HealthCheckResult> {
-    return this.healthCheckService.check([
-      (): Promise<HealthIndicatorResult> =>
-        this.databaseHealthIndicator.isHealthy('database'),
-      (): Promise<HealthIndicatorResult> =>
-        this.rabbitMQHealthIndicator.isHealthy('rabbitmq'),
-    ]);
+
+    const checks: (() => Promise<HealthIndicatorResult>)[] = [
+    () => this.rabbitMQHealthIndicator.isHealthy('rabbitmq'),
+    ];
+
+    if(this.databaseHealthIndicator) {
+      checks.push(() => this.databaseHealthIndicator.isHealthy('database'));
+    }
+
+    return this.healthCheckService.check(checks);
+    // return this.healthCheckService.check([
+    //   (): Promise<HealthIndicatorResult> =>
+    //     this.databaseHealthIndicator.isHealthy('database'),
+    //   (): Promise<HealthIndicatorResult> =>
+    //     this.rabbitMQHealthIndicator.isHealthy('rabbitmq'),
+    // ]);
   }
 }
